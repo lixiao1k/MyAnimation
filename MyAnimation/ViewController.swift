4d05b465b58bd3e6073376fe0666575b5146bfc1
李晓冬 <lixiao1k@163.com>
2017-09-26 19:03:52 +0800

    first_Animation_test

--- MyAnimation/ViewController.swift
+++ MyAnimation/ViewController.swift
@@ -10,14 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var switchBtn: UIButton!
+    @IBOutlet weak var img: UIImageView!
+    
+    @IBAction func `switch`(_ sender: UIButton) {
+        print("Hello");
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        addAnimation();
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func addAnimation() {
+        var imgArray:[UIImage]! = [];
+        for i in 0 ..< 58 {
+            let name:String! = "loading_page_"+String(format: "%05d", i);
+            let image: UIImage! = UIImage(named: name);
+            imgArray.append(image);
+        }
+        img.animationImages = imgArray;
+        img.animationRepeatCount = 0;
+        img.animationDuration = 3;
+        startAnimation();
+    }
+    func startAnimation() {
+        img.startAnimating();
+    }
+    
+    func stopAnimation(){
+        img.stopAnimating();
+    }
+    func open() {
     }
 
 
